d6ec74920ee62fc6fe65be4cb7a968d67a2c565c
Xigua2011 <67436019+Xigua2011@users.noreply.github.com>
2021-01-23 20:38:24 +0000

    sound of death, 3Dmodels

--- pirate_officer.gd
+++ pirate_officer.gd
@@ -20,4 +20,5 @@ func _ready():
 # warning-ignore:unused_argument
 func _on_pirate_officer_area_entered(area):
 	$AnimationPlayer.play("Head off")
+	$AudioStreamPlayer3D.play()
 	#queue_free() # Replace with function body.
