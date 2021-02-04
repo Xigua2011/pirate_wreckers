de8f376aefdddffae29338358c68530cbed868c5
Xigua2011 <67436019+Xigua2011@users.noreply.github.com>
2021-02-04 20:41:03 +0000

    add bullet and make it move

---
+++ Gun.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+
+var Bullet = preload("res://Bullet.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("VR_LEFT_INDEX_TRIGGER"):
+		var bullet = Bullet.instance()
+		bullet.transform = self.get_global_transform()
+#		bullet.translate(Vector3(0,10,0))
+		get_tree().current_scene.add_child(bullet)
+		print("bullet")
