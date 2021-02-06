5e2a713341edf73cc8d25fbf887ad88baff285f6
Xigua2011 <67436019+Xigua2011@users.noreply.github.com>
2021-02-06 19:51:57 +0000

    get bullet to right place and faster

--- Bullet.gd
+++ Bullet.gd
@@ -8,9 +8,9 @@ extends Area
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	translate(Vector3(0,0,-1))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	translate(Vector3(0,0,delta*2))
+	translate(Vector3(0,0,-delta*6))
