acff7d8e19206c11418f780dad5931f83fdb2c42
Xigua2011 <67436019+Xigua2011@users.noreply.github.com>
2021-02-13 20:19:39 +0000

    Physics working and faces to shoot

--- main.gd
+++ main.gd
@@ -13,19 +13,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if $"VR/RightController".get_is_active():
-		var x = $"VR/RightController".get_joystick_axis(0)
-		var y=$"VR/RightController".get_joystick_axis(1)
-		if y > 0.5:
-			print('Up')
-			$"VR".translation.y += 0.1
-		if y < -0.5:
-			print('down')
-			$"VR".translation.y += -0.1
-			
-	if Input.is_action_pressed("Move up"):
-		print("move up")
-		$"VR".translation.y += 0.1
-	if Input.is_action_pressed("Move down"):
-		print("move down")
-		$"VR".translation.y += -0.1
+	pass
+#	if $"VR/RightController".get_is_active():
+#		var x = $"VR/RightController".get_joystick_axis(0)
+#		var y=$"VR/RightController".get_joystick_axis(1)
+#		if y > 0.5:
+#			print('Up')
+#			$"VR".translation.y += 0.1
+#		if y < -0.5:
+#			print('down')
+#			$"VR".translation.y += -0.1
+#
+#	if Input.is_action_pressed("Move up"):
+#		print("move up")
+#		$"VR".translation.y += 0.1
+#	if Input.is_action_pressed("Move down"):
+#		print("move down")
+#		$"VR".translation.y += -0.1
